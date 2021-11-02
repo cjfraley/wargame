b303deb4b1f5583ff85948045d889e13b9c2a7f8
Charles Fraley <cfraley@cfraley-osx.local>
2021-11-02 00:29:03 -0500

    Expanded nation creation rules. Added csv support to rules assembler script

--- rules_assembler/scratch.py
+++ rules_assembler/scratch.py
@@ -1,15 +1,24 @@
-import yaml, sys
+import yaml, sys, csv
 
 def create_instructions(config, depth_level=0):
+    if depth_level>5:
+        depth_level=5
     output=""
+    if config is None:
+        return output
     if isinstance(config, dict):
         for key in config:
-            output+=("#"*(depth_level+1)+" "+key+"\n")
-            output+=(create_instructions(config[key],depth_level=depth_level+1))
+            if key!="col":
+                output+=("#"*(depth_level+1)+" "+key+"\n")
+                output+=(create_instructions(config[key],depth_level=depth_level+1))
+            else:
+                output+="<details>\n"
+                output+=(create_instructions(config[key],depth_level=depth_level+1))
+                output+="\n</details>\n"
     elif isinstance(config, list):
         for entry in config:
             entry_str=create_instructions(entry,depth_level=depth_level+1)
-            output+=(entry_str)
+            output+="\n"+(entry_str)+"\n"
     elif isinstance(config,str):
         if ".yaml" in config:
             with open(config, "r") as config_file:
@@ -24,6 +33,45 @@ def create_instructions(config, depth_level=0):
                         line="#"*depth_level+line
                     output+=line
                 # output+=instr_lines
+        elif ".csv" in config:
+            if "," in config:
+                config_split=config.split(",")
+                csv_filename=config_split[0]
+                rows=(int(config_split[1]),int(config_split[2]))
+                cols=(int(config_split[3]),int(config_split[4]))
+                csv_reader = csv.reader(open(config_split[0], encoding="UTF-8"))
+                splitline_added=False
+                for line_num,line in enumerate(csv_reader):
+                    if line_num>=rows[0] or rows[0]==-1:
+                        if line_num<=rows[1] or rows[1]==-1:
+                            outline="\n|"
+                            line_length=0
+                            for col_num, entry in enumerate(line):
+                                if col_num>=cols[0] or cols[0]==-1:
+                                    if col_num<=cols[1] or cols[1]==-1:
+                                        outline+=" "+entry+" |"
+                                        if not splitline_added:
+                                            line_length+=1
+                            output+=outline
+                            if not splitline_added:
+                                splitline= "\n|"+("-"*5+"|")*line_length
+                                output+=splitline
+                                splitline_added=True
+
+
+            else:
+                csv_reader = csv.reader(open(config, encoding="UTF-8"))
+
+                splitline_added=False
+                for line in csv_reader:
+                    outline="\n|"
+                    for entry in line:
+                        outline+=" "+entry+" |"
+                    output+=outline
+                    if not splitline_added:
+                        splitline= "\n|"+("-"*5+"|")*len(line)
+                        output+=splitline
+                        splitline_added=True
         else:
             output+=config
     else:
--- rules_config/00_Setup/00_CreateNation/hex_v1.md
+++ rules_config/00_Setup/00_CreateNation/hex_v1.md
@@ -2,4 +2,3 @@
 1. Fill out nation sheet
 2. Turn corresponding tiles face up
 3. Place tiles in your continent tray as you wish
-
